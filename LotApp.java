c4a3f5fe1d8799bb743e55d4f8fb12c30a1bb7fc
hogehoge <elementrd6@hotmail.com>
2022-11-07 09:10:52 +0900

    first commit

---
+++ LotApp.java
@@ -0,0 +1,37 @@
+import java.util.*;
+/**
+ * LotApp
+ */
+public class LotApp {
+
+    public static void main(String[] args) {
+    
+    System.out.print("宝くじを何枚買いますか？>>");   
+    Scanner sc=new Scanner(System.in);
+    int select=sc.nextInt(); 
+    Random rand=new Random();
+    int pickedNums[]=new int[select];
+    int selectedNums[]=new int[4];
+      
+   //1回のみの0000-9999のランダム処理
+   for(int i=0;i<select;i++){
+    int pickedNum=rand.nextInt(9999);
+    
+    pickedNums[i]=pickedNum;
+    
+   }  
+   System.out.println(Arrays.toString(pickedNums));  
+   System.out.println("抽選開始...");
+   for(int i=0;i<4;i++){
+    int selectedNum=rand.nextInt(9);
+    System.out.println(selectedNum);
+    selectedNums[i]=selectedNum;
+    try{Thread.sleep(2000);
+    }catch(InterruptedException e){}
+   }
+   System.out.println("1等賞"+Arrays.toString(selectedNums).replace(",", ""));
+   System.out.println("2等賞");
+   System.out.println("3等賞");
+   System.out.println("4等賞");
+    }
+}
